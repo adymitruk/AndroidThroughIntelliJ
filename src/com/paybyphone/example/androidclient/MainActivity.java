aeba7dd5f3cbd1595b77514f76ee51b59e41cafa
Adam Dymitruk <Adam@Dymitruk.com>
2013-03-04 11:57:57 -0800

    MAPI-112 using restlets to hit the server.

--- src/com/paybyphone/example/androidclient/MainActivity.java
+++ src/com/paybyphone/example/androidclient/MainActivity.java
@@ -2,6 +2,12 @@ package com.paybyphone.example.androidclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.restlet.Client;
+import org.restlet.Context;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.data.Protocol;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.resource.ClientResource;
 
 public class MainActivity extends Activity {
     /**
@@ -9,6 +15,14 @@ public class MainActivity extends Activity {
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Client client = new Client(new Context(), Protocol.HTTP);
+        client.getContext().getParameters().add("useForwardedForHeader", "false");
+
+        ClientResource clientResource = new ClientResource("https://devapi.paybyphone.com:11443/v1/payments/generatetoken?amount/12.34");
+        clientResource.setRequestEntityBuffering(true);
+        clientResource.setNext(client);
+        clientResource.get();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
