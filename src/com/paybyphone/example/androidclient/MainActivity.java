795ba9d80f637e07a631011f1815bddf99584ca5
Adam Dymitruk <Adam@Dymitruk.com>
2013-03-12 17:53:32 -0700

    MAPI-112 clean up code. Outstanding is the other URL not working.

--- src/com/paybyphone/example/androidclient/MainActivity.java
+++ src/com/paybyphone/example/androidclient/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.google.gson.Gson;
 import org.restlet.Client;
 import org.restlet.Request;
@@ -22,25 +23,18 @@ import org.restlet.ext.ssl.HttpsClientHelper;
 public class MainActivity extends Activity {
 
     // Use AsyncTask to avoid using the UI thread to perform long running tasks such as network calls
-    class FetchUri extends AsyncTask<String, Void, String> {
+    class FetchUri extends AsyncTask<Void, Void, String> {
         private String resourceUri;
         public FetchUri(String resourceUri) { this.resourceUri = resourceUri; }
+
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(Void... params) {
             writeLineResult("Started background task to get url");
             try {
-                Engine.getInstance().getRegisteredClients().clear();
-                Engine.getInstance().getRegisteredClients().add(new HttpsClientHelper(null));
-
-                Gson gson = new Gson();
-                writeLineResult("created gson");
-                Client client = new Client(Protocol.HTTPS);
-                writeLineResult("about to call server with " + resourceUri);
-                Response response = client.handle(new Request(Method.GET, resourceUri));
+                Response response = new Client(Protocol.HTTPS).handle(new Request(Method.GET, resourceUri));
+                if (!response.getStatus().isSuccess()) throw new Exception("Request failed");
                 String urlJson = response.getEntityAsText();
-                writeLineResult("returned json: " + urlJson);
-                paymentURL = gson.fromJson(urlJson, UrlForPaying.class);
-                writeLineResult("url extracted from json: " + paymentURL.getUrl());
+                paymentURL = new Gson().fromJson(urlJson, UrlForPaying.class);
             } catch (Exception e) {
                 writeLineResult("error type: " + e.getClass().toString());
                 writeLineResult("error message: " + e.getMessage());
@@ -49,11 +43,8 @@ public class MainActivity extends Activity {
         }
         @Override
         protected void onPostExecute(String result) {
-            writeLineResult("got the post execute called with " + result);
             resultText.setText(result);
-            writeLineResult("showed the result");
             getTokenButton.setEnabled(true);
-            writeLineResult("enabled button");
         }
     }
     @Override
@@ -63,23 +54,25 @@ public class MainActivity extends Activity {
         getTokenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            writeLineResult("clicked token request button\n");
-            getTokenButton.setEnabled(false);
-            writeLineResult("disabled button");
-            String resourceUri = TOKEN_URI + "/?amount=" + Uri.encode(amountInput.getText().toString()) +
-                    "&phone=" + Uri.encode(phoneInput.getText().toString()) +
-                    "&yourpaymentref=" + Uri.encode(refInput.getText().toString()) +
-                    "&name=" + Uri.encode(nameInput.getText().toString()) +
-                    "&email=" + Uri.encode(emailInput.getText().toString());
-            new FetchUri(resourceUri).execute("");
-            writeLineResult("executed async process. Leaving click listener.");
-        }});
+                getTokenButton.setEnabled(false);
+                String resourceUri = TOKEN_URI + "/?amount=" + Uri.encode(amountInput.getText().toString()) +
+                        "&phone=" + Uri.encode(phoneInput.getText().toString()) +
+                        "&yourpaymentref=" + Uri.encode(refInput.getText().toString()) +
+                        "&name=" + Uri.encode(nameInput.getText().toString()) +
+                        "&email=" + Uri.encode(emailInput.getText().toString());
+                new FetchUri(resourceUri).execute();
+            }
+        });
         payForCabButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(paymentURL.getUrl()));
-            startActivity(intent);
+                if (paymentURL == null || paymentURL.getUrl() == null || paymentURL.getUrl() == "") {
+                    Toast.makeText(getApplicationContext(), "Please request a token first.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(paymentURL.getUrl()));
+                startActivity(intent);
             }
         });
     }
@@ -87,6 +80,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        Engine.getInstance().getRegisteredClients().clear();
+        Engine.getInstance().getRegisteredClients().add(new HttpsClientHelper(null));
 
         payForCabButton = (Button) findViewById(R.id.startWebPage);
         getTokenButton = (Button) findViewById(R.id.tokenRequestButton);
